70d0ae157df6be099adf3628d50978db528c4581
gajevski <mikolajxgajewski@gmail.com>
2024-07-03 13:02:58 +0200

    clean main.go

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
-	"net/url"
-	"strings"
+	pokemonList "pokedex/controllers"
+	"pokedex/shared/utils"
 
 	"github.com/mtslzr/pokeapi-go"
 	"github.com/mtslzr/pokeapi-go/structs"
@@ -15,17 +14,6 @@ import (
 type PokemonDetailsData struct {
 	Evolutions structs.EvolutionChain
 	Pokemon    structs.Pokemon
-}
-
-func landingPageHandler(w http.ResponseWriter, r *http.Request) {
-	pokemonData, err := pokeapi.Resource("pokemon", 0, 905)
-	if err != nil {
-		log.Panic("error fetching pokeapi")
-	}
-	templ := template.Must(template.New("index.html").Funcs(template.FuncMap{
-		"extractId": extractId,
-	}).ParseFiles("./templates/index.html"))
-	templ.Execute(w, pokemonData)
 }
 
 func pokemonPageHandler(w http.ResponseWriter, r *http.Request) {
@@ -45,7 +33,7 @@ func pokemonPageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Pokemon species not found", http.StatusNotFound)
 		return
 	}
-	evolutionId, err := extractId(pokemonSpecies.EvolutionChain.URL)
+	evolutionId, err := utils.ExtractID(pokemonSpecies.EvolutionChain.URL)
 	if err != nil {
 		http.Error(w, "Evolution chain URL not found", http.StatusNotFound)
 		return
@@ -62,53 +50,18 @@ func pokemonPageHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	templ := template.Must(template.New("pokemon.html").Funcs(template.FuncMap{
-		"incrementId":         incrementId,
-		"decrementId":         decrementId,
-		"extractId":           extractId,
-		"preparePokemonImage": preparePokemonImage,
+		"incrementId":         utils.IncrementID,
+		"decrementId":         utils.DecrementID,
+		"extractId":           utils.ExtractID,
+		"preparePokemonImage": utils.PreparePokemonImage,
 	}).ParseFiles("./templates/pokemon.html"))
 
 	templ.Execute(w, pokemonDetailsData)
 }
 
 func main() {
-	http.HandleFunc("GET /{$}", landingPageHandler)
+	http.HandleFunc("GET /{$}", pokemonList.PokemonListHandler)
 	http.HandleFunc("GET /pokemon/{name}", pokemonPageHandler)
 	fmt.Println("Server running on port 8000")
 	http.ListenAndServe(":8000", nil)
-}
-
-func extractId(urlString string) (string, error) {
-	u, err := url.Parse(urlString)
-	if err != nil {
-		fmt.Println("Error parsing URL:", err)
-		return "", err
-	}
-	path := u.Path
-	parts := strings.Split(path, "/")
-	if len(parts) > 1 {
-		number := parts[len(parts)-2]
-		return number, nil
-	} else {
-		fmt.Println("URL path format is incorrect")
-	}
-	return "", nil
-}
-
-func incrementId(id int) int {
-	return id + 1
-}
-
-func decrementId(id int) int {
-	return id - 1
-}
-
-func preparePokemonImage(id int) string {
-	if id < 10 {
-		return fmt.Sprintf("00%d", id)
-	}
-	if id < 100 && id >= 10 {
-		return fmt.Sprintf("0%d", id)
-	}
-	return fmt.Sprintf("%d", id)
 }
