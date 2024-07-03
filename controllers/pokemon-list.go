1ae9d22b9290e588ea2b6ecf57fef937b2efb762
gajevski <mikolajxgajewski@gmail.com>
2024-07-03 13:07:00 +0200

    cleanup pokemon list controller

--- controllers/pokemon-list.go
+++ controllers/pokemon-list.go
@@ -1,19 +1,13 @@
 package pokemonList
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"strings"
+	"pokedex/shared/utils"
 	"text/template"
 
 	"github.com/mtslzr/pokeapi-go"
 )
-
-func Greet() {
-	fmt.Println("Hello from pokemonList")
-}
 
 func PokemonListHandler(w http.ResponseWriter, r *http.Request) {
 	pokemonData, err := pokeapi.Resource("pokemon", 0, 905)
@@ -21,24 +15,7 @@ func PokemonListHandler(w http.ResponseWriter, r *http.Request) {
 		log.Panic("error fetching pokeapi")
 	}
 	templ := template.Must(template.New("index.html").Funcs(template.FuncMap{
-		"extractId": extractId,
+		"extractId": utils.ExtractID,
 	}).ParseFiles("./templates/index.html"))
 	templ.Execute(w, pokemonData)
-}
-
-func extractId(urlString string) (string, error) {
-	u, err := url.Parse(urlString)
-	if err != nil {
-		fmt.Println("Error parsing URL:", err)
-		return "", err
-	}
-	path := u.Path
-	parts := strings.Split(path, "/")
-	if len(parts) > 1 {
-		number := parts[len(parts)-2]
-		return number, nil
-	} else {
-		fmt.Println("URL path format is incorrect")
-	}
-	return "", nil
 }
